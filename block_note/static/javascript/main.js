6e14c4f3361e88e6451ff3900db7e33b2e12b6c0
Djegnene babo penyel karlenry <66698773+quantumporium@users.noreply.github.com>
2021-01-26 20:32:29 -0500

    Add files via upload

---
+++ block_note/static/javascript/main.js
@@ -0,0 +1,7 @@
+transaction_array = []
+
+// use flask instead to show transaction
+
+function make_block(){
+    alert('A new block was created you can go see it at 127.0.0.1:8000/see_block.html')
+}
